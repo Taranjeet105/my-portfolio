2755db3967067f755b0f3b30d94b33b45a1e7b00
taranjeet <shantys502@gmail.com>
2021-08-19 22:40:39 +0530

    make it better

---
+++ main.js
@@ -0,0 +1,15 @@
+const express=require('express')
+const bodyParser=require('body-parser')
+const app=express()
+const PORT= process.env.PORT || 3000
+const authRoutes=require('./routes/authroutes')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.set('view engine','ejs')
+app.use(authRoutes)
+
+app.listen(PORT,()=>{
+    console.log("server running on PORT "+PORT)
+})
---
+++ routes/authroutes.js
@@ -0,0 +1,55 @@
+const express=require('express')
+const router=express.Router()
+const nodemailer=require("nodemailer")
+router.get('/',(req,res)=>{
+    res.render("index")
+  })
+  
+router.post('/contact',(req,res)=>{
+    console.log(req.body)
+
+    var message=req.body.message;
+    var senderEmail=req.body.email;
+    var name=req.body.name;
+
+    //SENDING THE MAIL TO VIKASHP0901@GMAIL.COM
+    var transporter = nodemailer.createTransport({
+        service: "Gmail",
+        auth: {
+          // user: process.env.EMAIL,
+          // pass: process.env.PASSWORD,
+          user: 'shantys502@gmail.com',
+          pass: 'daiusokngmqktgup',
+        },
+    });
+
+    var mailOptions = {
+        from: 'shantys502@gmail.com',
+        to: 'shantys502@gmail.com',
+        subject: "User Query on Taranjeet singh portfolio",
+        html: `
+        Name of the sender : ${name}
+        <br/>
+        Email of the sender : ${senderEmail}
+        <br/>
+        Message : ${message}
+        `,
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+          return res.json({success:false,message:error})
+        } else {
+          console.log("Email sent: " + info.response);
+        return res.json({success:true,message:"message sent"})
+        }
+    });
+
+
+
+    // res.json("thanks for contacting")
+})
+
+
+module.exports=router
