1d670438f0a4ac8d7954711361e12aeb57e01ad5
taranjeet <shantys502@gmail.com>
2021-08-20 12:39:09 +0530

    changes

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const express=require('express')
 const bodyParser=require('body-parser')
+const cors = require('cors');
 const app=express()
 const PORT= process.env.PORT || 3000
 const authRoutes=require('./routes/authroutes')
-
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}))
 
