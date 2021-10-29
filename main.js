9ce76ec73dd4cabeab645f0df912e9fe6426b54a
Taranjeet105 <shantys502@gmail.com>
2021-10-29 21:02:44 +0530

    UI improved

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 const express=require('express')
+require('dotenv').config()
 const bodyParser=require('body-parser')
 const cors = require('cors');
 const app=express()
+const path=require('path')
 const PORT= process.env.PORT || 3000
 const authRoutes=require('./routes/authroutes')
 app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}))
-
+let public=path.join(__dirname,'./public')
+console.log(public)
+app.use(express.static(public))
 app.set('view engine','ejs')
 app.use(authRoutes)
 
